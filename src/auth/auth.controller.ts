d48208bfd9b0aa27a1c634055cbdb046a5275157
Harold C.S. Becker <hcsb20@gmail.com>
2025-05-15 18:06:06 -0300

    feat: Create user auth with jwt

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body, UseGuards, Req, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalGuard } from './guards/local.guard';
+import { Request } from 'express';
+import { JwtGuard } from './guards/jwt.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  @UseGuards(LocalGuard)
+  login(@Req() req: Request) {
+    return req.user;
+  }
+
+  @Get('profile')
+  @UseGuards(JwtGuard)
+  status(@Req() req: Request) {
+    return req.user;
+  }
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', enum: ['admin', 'user'] })
+  role: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
