5d67e15439388fd89be02fb432c1f31d72614d40
Harold C.S. Becker <hcsb20@gmail.com>
2025-05-17 16:02:15 -0300

    feat: Create role based endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,29 +1,47 @@
-import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Role } from 'src/common/enum/role.enum';
 import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { RolesGuard } from 'src/auth/guards/roles/roles.guard';
 
+@Roles(Role.USER, Role.ADMIN)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
   @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtGuard)
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
