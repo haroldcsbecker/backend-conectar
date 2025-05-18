161095bb975fa5a0145108dc1f6c457d63721e02
Harold C.S. Becker <hcsb20@gmail.com>
2025-05-18 18:00:09 -0300

    feat: Add route validation

--- src/auth/dto/auth-jwt-payload.dto.ts
+++ src/auth/dto/auth-jwt-payload.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class AuthJwtPayloadDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   role: string;
 }
